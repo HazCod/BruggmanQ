2ab70bebd40369d8ab7e57964d45d170a7387495
HazCod <hazcod@outlook.be>
2014-07-31 11:51:06 +0200

    Update config.php
    Removed obsolete data.

--- application/config.php
+++ application/config.php
@@ -4,13 +4,13 @@ date_default_timezone_set('Europe/Brussels');
 
 $db_config = array(
     'driver' => 'mysql',
-    'username' => 'c7185zrc_bgU',
-    'password' => 'HP35asmJ',
-    'schema' => 'c7185zrc_bg',
+    'username' => '',
+    'password' => '',
+    'schema' => '',
     
     'dsn' => array(
         'host' => 'localhost',
-        'dbname' => 'c7185zrc_bg',
+        'dbname' => '',
         'port' => '3306',
     )
 );
